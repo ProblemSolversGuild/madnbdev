bb00b6b4726ce44df0796ab6f9588c7157b86f41
Kevin Bird <kevinbird15@gmail.com>
2023-10-30 02:05:40 +0000

    initial push

--- madpy/intro.py
+++ madpy/intro.py
@@ -3,12 +3,12 @@
 # %% auto 0
 __all__ = ['arrange_talk']
 
-# %% ../nbs/00_introduction.ipynb 4
+# %% ../nbs/00_introduction.ipynb 14
 import datetime
 
-# %% ../nbs/00_introduction.ipynb 5
+# %% ../nbs/00_introduction.ipynb 16
 def arrange_talk(speaker:str, # The name of the speaker
                  title:str, # The title of the talk that will be given
-                 talk_date:datetime.date # The date that the speaker will be giving the talk
+                 talk_date:datetime.date=datetime.date.today() # The date that the speaker will be giving the talk
                 ) -> str: # The output is a string that gives a human readable output of the information provided
     return f"{speaker} will be talking about {title} on {talk_date}"
