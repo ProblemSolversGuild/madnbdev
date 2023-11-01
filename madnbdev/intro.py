4a738e72c42df42cfc989264f090c6aba49ea2f2
Kevin <kevinbird15@gmail.com>
2023-11-01 02:23:31 +0000

    test slides

--- madnbdev/intro.py
+++ madnbdev/intro.py
@@ -3,10 +3,10 @@
 # %% auto 0
 __all__ = ['arrange_talk']
 
-# %% ../nbs/00_introduction.ipynb 16
+# %% ../nbs/00_introduction.ipynb 20
 import datetime
 
-# %% ../nbs/00_introduction.ipynb 18
+# %% ../nbs/00_introduction.ipynb 22
 def arrange_talk(speaker:str, # The name of the speaker
                  title:str, # The title of the talk that will be given
                  talk_date:datetime.date=datetime.date.today() # The date that the speaker will be giving the talk
